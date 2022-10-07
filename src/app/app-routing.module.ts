54da44f5c44fdaa2c6f72e766daaee76c178cf01
David Fonseca <fonsecasoundcruz@gmail.com>
2022-10-07 08:36:29 -0500

    Crear proyecto, menu y componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrearGrupoComponent } from './administrador/grupos/crear-grupo/crear-grupo.component';
+import { CrearMateriaComponent } from './administrador/materias/crear-materia/crear-materia.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'grupos/crear',
+    component: CrearGrupoComponent
+  },
+  {
+    path: '',
+    component: CrearMateriaComponent
+  },
+  // home page
+  {
+    path: '*', redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from "./material/material.module";
+import {MenuComponent}from "./menu/menu.component";
+import { CrearMateriaComponent } from './administrador/materias/crear-materia/crear-materia.component';
+import { CrearGrupoComponent } from './administrador/grupos/crear-grupo/crear-grupo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CrearMateriaComponent,
+    CrearGrupoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
